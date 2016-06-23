90a0af87261fcb403c12441a45d52f5cd32e3e1e
zhihuazhang <zhangzhihua.dev@gmail.com>
2016-06-23 10:03:44 +0900

    Initial commit

---
+++ Example/KACheckList/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KACheckList
+//
+//  Created by zhihuazhang on 06/23/2016.
+//  Copyright (c) 2016 zhihuazhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
